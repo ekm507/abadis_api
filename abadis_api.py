eabff02aa5692a6b66b865c36da3aca523f481a3
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-04-28 10:25:21 +0430

    text_splitter

--- abadis_api.py
+++ abadis_api.py
@@ -12,3 +12,13 @@ def get_translation(word, lang_from='en', lang_to='fa'):
     translate_request = requests.post(link, data=data)
     return translate_request.text
 
+def text_splitter(text:str) -> list:
+    lines = text.split('^')
+
+    output = []
+    for line in lines:
+        line_parts = line.split('#')
+        if len(line_parts) > 1:
+            output.append(line_parts)
+    
+    return output
